e7b899247094086f5046e32fb11b1c6eff79d25f
Tyrn <a@b.com>
2020-08-30 16:13:09 +0300

    Major refactoring

--- tyrn_switch/tys.py
+++ tyrn_switch/tys.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+""" Setting links, maybe system variables, etc.,
+    according to command line options.
+"""
+
 import sys
 
 if sys.version_info < (3, 6, 0):
@@ -9,98 +13,110 @@ if sys.version_info < (3, 6, 0):
 import os
 import argparse
 import warnings
-from pathlib import Path
-
 
 LSHARE = "~/.local/share"
 CONF = "~/.config"
-TARGET = f"{CONF}/arch-chronicle"
 
-
-AG = None
+LN_PATHS = [
+    (f"{CONF}/arch-chronicle", f"{CONF}/nvim"),
+    (LSHARE, f"{LSHARE}/nvim"),
+]
 
 
 def list_links():
-    os.system(f"ls -l {CONF}/nvim")
-    os.system(f"ls -l {LSHARE}/nvim")
+    """ Lists the known links.
+    """
+    for _, link in LN_PATHS:
+        os.system(f"ls -l {link}")
 
 
 def remove_links():
+    """ Removes the known links.
+    """
     print("To be removed: ***********")
     list_links()
-    os.system(f"rm {CONF}/nvim")
-    os.system(f"rm {LSHARE}/nvim")
+    for _, link in LN_PATHS:
+        os.system(f"rm {link}")
     print("**************************\n")
 
 
-def create_links_to_nvim(target):
+def create_links(target):
+    """ Creates the required links to [target].
+    """
     remove_links()
-    os.system(f"ln -s {TARGET}/{target} {CONF}/nvim")
-    os.system(f"ln -s {LSHARE}/{target} {LSHARE}/nvim")
+    for target_base, link in LN_PATHS:
+        os.system(f"ln -s {target_base}/{target} {link}")
     list_links()
 
 
-def options():
-    """
-    Processes selected options.
-    """
-    if AG.list_options:
-        list_links()
-    elif AG.remove_links:
-        remove_links()
-    elif AG.nvim_fisa:
-        create_links_to_nvim("nvim-fisa")
-    elif AG.nvim_gq:
-        create_links_to_nvim("nvim-gq")
+# Option names.
+ARG_NVIM_FISA = "-fa"
+ARG_NVIM_GQ = "-gq"
+ARG_LIST_OPTIONS = "-ls"
+ARG_REMOVE_LINKS = "-rl"
 
-
-def retrieve_args():
-    """
-    Creates and reads command line options.
-    """
-    parser = argparse.ArgumentParser(
-        description="""
-Inhouse options: switching bits and pieces.
-    """
-    )
-
-    parser.add_argument(
-        "-fa", "--nvim-fisa", help="select fisa neovim config", action="store_true",
-    )
-    parser.add_argument(
-        "-gq", "--nvim-gq", help="select gq neovim config", action="store_true",
-    )
-    parser.add_argument(
-        "-ls",
-        "--list-options",
-        help="list all the options as selected",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-rl",
-        "--remove-links",
-        help="remove option defining soft links",
-        action="store_true",
-    )
-    return parser.parse_args()
+VALID_ARGS = {
+    ARG_NVIM_FISA: lambda: create_links("nvim-fisa"),
+    ARG_NVIM_GQ: lambda: create_links("nvim-gq"),
+    ARG_LIST_OPTIONS: list_links,
+    ARG_REMOVE_LINKS: remove_links,
+}
 
 
 def main():
-    """
-    The script entry point.
-    Reads command line options
+    """ The script entry point. Reads command line options
     and runs the script.
     """
-    global AG
-
     try:
         warnings.resetwarnings()
         warnings.simplefilter("ignore")
 
-        AG = retrieve_args()
-        options()
+        VALID_ARGS[retrieve_arg()]()
     except KeyboardInterrupt as ex:
         sys.exit(ex)
+
+
+def retrieve_arg():
+    """ Creates and reads command line options.
+    """
+    parser = argparse.ArgumentParser(
+        description="""Inhouse options: switching bits and pieces."""
+    )
+
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument(
+        ARG_NVIM_FISA,
+        help="select fisa neovim config",
+        const=ARG_NVIM_FISA,
+        type=str,
+        nargs="?",
+    )
+    group.add_argument(
+        ARG_NVIM_GQ,
+        help="select gq neovim config",
+        const=ARG_NVIM_GQ,
+        type=str,
+        nargs="?",
+    )
+    group.add_argument(
+        ARG_LIST_OPTIONS,
+        help="list all the options as selected",
+        const=ARG_LIST_OPTIONS,
+        type=str,
+        nargs="?",
+    )
+    group.add_argument(
+        ARG_REMOVE_LINKS,
+        help="remove option defining soft links",
+        const=ARG_REMOVE_LINKS,
+        type=str,
+        nargs="?",
+    )
+    args = parser.parse_args()
+    for _, value in args.__dict__.items():
+        if value in VALID_ARGS.keys():
+            return value
+    return ARG_LIST_OPTIONS  # Listing is always harmless.
 
 
 if __name__ == "__main__":
